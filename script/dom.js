54dd10bf58388bfbf2cbcac300703d4b1f5996fb
Gwerborc'h Mahéo <gwerborch.maheo.com>
2020-11-25 11:30:00 +0100

    définition en commentaire des étapes js à effectuer

--- script/dom.js
+++ script/dom.js
@@ -1,5 +1,27 @@
-let rap = [];
-let variété = [];
-let dance = [];
+// let rap = [];
+// let variété = [];
+// let dance = [];
+
+// Qu'est-ce que je veux ?
+
+// 1) regrouper les musiques dans un tableau
+
+// 2)Display les musiques a partir du moment où l'on commence
+//  2bis) Si j'ai le temps, les displays de manière aléatoire
+
+//3)Afficher les 3 possibilités de réponse en fonction de la musique qui passe
+
+// 4)Ajouter un evênement : si
+//      on click sur la bonne musique gagné
+//      on click sur la mauvaise musique = perdu
+
+// 5bis) (option) Ajouter un truc marrant en fonction de gagné ou perdu
+
+// un seul essai, ça sera plus simple, le but étant de réussir à toutes les musiques pour avancer, sinon on recommence
+        // si cette option choisi, les display de manière aléatoire devient plus interressant
+
+// 6) réaliser un timer qui démarre au commencement de la musique (définir le temps)
+//      soit :
+//          si temps écoulé, perdu...
 
 
